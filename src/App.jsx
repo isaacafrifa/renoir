124370dcdfee4af05052d525d0556bb9926e00fb
Afrifa <isaac.afrifa3@yahoo.com>
2024-01-28 20:52:04 +0200

    refactor: remove unwanted default code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './assets/css/App.css'
 
 function App() {
   
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <h1>Vite + React</h1>
+      <div className='app'>
+           <h1>Renoir</h1>
       </div> 
     </>
   )
