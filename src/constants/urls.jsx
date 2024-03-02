703eb3a57059f1525e3f30263bb4788595f74819
Afrifa <isaac.afrifa3@yahoo.com>
2024-03-02 15:08:41 +0200

    ✅ feat: implement menu page

--- src/constants/urls.jsx
+++ src/constants/urls.jsx
@@ -2,3 +2,4 @@ export const HOME_URL = "/";
 export const MENU_URL = "/menu";
 export const ABOUT_URL = "/about";
 export const CONTACT_URL = "/contact";
+export const RENOIR_MENU_URL= import.meta.env.VITE_RENOIR_MENU_URL;
