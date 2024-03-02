0e8f4d93b4778dc909c5861d580eaac4f8fccfcc
Afrifa <isaac.afrifa3@yahoo.com>
2024-03-02 14:28:39 +0200

    ✅feat: implement the about page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,28 +2,45 @@ import { Outlet, NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import "../assets/css/navbar.css"
-
+import "../assets/css/navbar.css";
 
 export default function Layout() {
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" fixed="top">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="bg-body-tertiary"
+        fixed="top">
         <Container>
           <Navbar.Brand href="/">Renoir</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <NavLink to="/about" activeClassName="active" className="nav-links">About</NavLink>
+              <NavLink
+                to="/about"
+                activeClassName="active"
+                className="nav-links">
+                About
+              </NavLink>
             </Nav>
             <Nav>
-              <NavLink to="/menu" activeClassName="active" className="nav-links">Menu</NavLink>
-              <NavLink to="/contact" activeClassName="active" className="nav-links">Contact</NavLink>
+              <NavLink
+                to="/menu"
+                activeClassName="active"
+                className="nav-links">
+                Menu
+              </NavLink>
+              <NavLink
+                to="/contact"
+                activeClassName="active"
+                className="nav-links">
+                Contact
+              </NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <hr />
       <Outlet />
     </div>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,23 +1,55 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import "../assets/css/about.css";
+import Table from "react-bootstrap/Table";
 
 const About = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h1>About</h1>
-          <p>
-            Donec sit amet augue at enim sollicitudin porta. Praesent finibus ex
-            velit, quis faucibus libero congue et. Quisque convallis eu nisl et
-            congue. Vivamus eget augue quis ante malesuada ullamcorper. Sed orci
-            nulla, eleifend eget dui faucibus, facilisis aliquet ante.
-            Suspendisse sollicitudin nibh lacus, ut bibendum risus elementum a.
-          </p>
-        </Col>
-      </Row>
-    </Container>
+    <div className="page-container">
+      <Container fluid>
+        <Row>
+          <Col md={12}>
+            <div className="page-banner">
+              <div className="banner-text">
+                <h1 className="animate__animated animate__fadeInDown">Our Story</h1>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} lg={{ span: 6, offset: 3 }}>
+            <div className="about-us">
+              <p className="animate__animated animate__fadeInDown">
+                Welcome to Renoir, Celebrating the Rich and Diverse Flavours of
+                Africa & Beyond. Established in 2022 in the heart of Accra, our
+                restaurant is a tribute to the rich culinary heritage of Ghana
+                and the continent as a whole. We are passionate about bringing
+                the vibrant tastes of Africa and beyond to your plate. We strive
+                to create an unforgettable dining experience for our guests.
+                From the moment you step through our doors, you will embark on a
+                gastronomic journey that explores the authentic flavours and
+                culinary traditions of Africa.
+              </p>
+              <div className="mt-5 text-center">
+                <h2 className="animate__animated animate__fadeInDown animate__slow">Working hours</h2>
+
+                <Table responsive className="animate__animated animate__fadeInDown animate__slow">
+                  <tbody>
+                    <tr>
+                      <td>Monday to Saturday</td>
+                      <td>12pm to 10pm</td>
+                    </tr>
+                  </tbody>
+                </Table>
+
+                <p className="fw-light text-body-tertiary animate__animated animate__fadeInDown animate__slower">
+                  (Including holidays)
+                </p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
