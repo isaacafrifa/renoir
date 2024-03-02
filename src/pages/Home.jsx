acdd5a25c5262e890f38b565b07dc1ad15f17268
Afrifa <isaac.afrifa3@yahoo.com>
2024-03-03 00:57:57 +0200

    🎨 style(home): fill 100% of width

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,23 +5,15 @@ import "../assets/css/home.css";
 
 const Home = () => {
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <div>
-            <div className="bg"></div>
-            <div className="bg-text">
-              
-              <h1 className="animate__animated animate__fadeInDown">Renoir</h1>
-              <p className="animate__animated animate__fadeInDown">
-                Renoir is an afro-french restaurant that serves authentic and
-                delicious local and continental dishes.
-              </p>
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <div className="bg">
+      <div className="bg-text">
+        <h1 className="animate__animated animate__fadeInDown">Renoir</h1>
+        <p className="animate__animated animate__fadeInDown">
+          Renoir is an afro-french restaurant that serves authentic and
+          delicious local and continental dishes.
+        </p>
+      </div>
+    </div>
   );
 };
 
