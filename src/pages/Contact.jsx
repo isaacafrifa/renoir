d70c813dc383b8feddc45e990e781889950ef889
Afrifa <isaac.afrifa3@yahoo.com>
2024-03-03 14:01:29 +0200

    🎨 style: make banner fill 100% width on contact and about pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,50 +15,47 @@ import ContactForm from "../components/ContactForm";
 const Contact = () => {
   return (
     <div className="page-container">
+      <div className="page-banner">
+        <div className="banner-text">
+          <h1 className="animate__animated animate__fadeInDown">Contact Us</h1>
+        </div>
+      </div>
+
       <Container fluid>
         <Row>
-          <Col md={12}>
-            <div className="page-banner">
-              <div className="banner-text">
-                <h1 className="animate__animated animate__fadeInDown">
-                  Contact Us
-                </h1>
-              </div>
-            </div>
-          </Col>
           <Col md={6}>
             <Card className="address-card">
               <div className="text-md-end card-content">
-              <p className="animate__animated animate__fadeInDown">
-                    <ClockFill color="green" /> <span className="text-success">Mon to Sat:</span> 12pm to 10pm
-                  </p>
-                  <p className="animate__animated animate__fadeInDown">
-                    <GeoAltFill color="green" /> 88 Konstiole Rd. Labone - Accra,
-                    Ghana
-                  </p>
-                  <p className="animate__animated animate__fadeInDown">
-                    <TelephoneFill color="green" /> 0302956232
-                  </p>
-                  <a
-                    className="icon-box animate__animated animate__fadeInDown"
-                    title="Facebook"
-                    href="#">
-                    <Facebook size={24} />
-                  </a>
-                  <a
-                    className="icon-box animate__animated animate__fadeInDown"
-                    title="Instagram"
-                    href="#">
-                    <Instagram size={24} />
-                  </a>
+                <p className="animate__animated animate__fadeInDown">
+                  <ClockFill color="green" />{" "}
+                  Mon to Sat: 12pm to 10pm
+                </p>
+                <p className="animate__animated animate__fadeInDown">
+                  <GeoAltFill color="green" /> 88 Konstiole Rd. Labone - Accra,
+                  Ghana
+                </p>
+                <p className="animate__animated animate__fadeInDown">
+                  <TelephoneFill color="green" /> 0302956232
+                </p>
+                <a
+                  className="icon-box animate__animated animate__fadeInDown"
+                  title="Facebook"
+                  href="#">
+                  <Facebook size={24} />
+                </a>
+                <a
+                  className="icon-box animate__animated animate__fadeInDown"
+                  title="Instagram"
+                  href="#">
+                  <Instagram size={24} />
+                </a>
               </div>
             </Card>
           </Col>
 
           <Col md={6}>
-          <ContactForm />
+            <ContactForm />
           </Col>
-          
         </Row>
       </Container>
     </div>
